88d714a2a413cd28d297beb3943d5fb62690c6e6
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-16 01:24:39 -0300

    feat: added collor pallet

--- src/Styles/GlobalStyles/index.js
+++ src/Styles/GlobalStyles/index.js
@@ -7,6 +7,20 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     text-decoration: none;
     }
+    
+    :root {
+        --green-pallet-one: #60B948;
+        --green-pallet-two: #65BD5A;
+        --green-pallet-three: #84C67C;
+        --green-pallet-four: #E9F5DB;
+
+        --font-primary: #313131;
+        --font-primary-light: #656565;
+        --font-secondary: #FFFFFF;
+        --alert-error: #FF5555; 
+        --alert-succes: #81FF64;
+        --bg-with-transp: rgba(0, 0, 0, 0.61);
+    }
 `;
 
 export default GlobalStyles;
