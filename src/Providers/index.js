9a3886524d4ea62f740ed2ff76f35f34128b65d5
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-18 19:50:14 -0300

    feat: imported Login and Activities contexts

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -1,14 +1,18 @@
-import { RegisterContext } from "./register"
-import { UserHabits } from "./userHabits" 
+import { RegisterContext } from "./register";
+import { LoginContext } from "./login";
+import { UserHabits } from "./userHabits";
+import { ActivitiesContext } from "./activities";
 
 const ProvidersGathered = ({ children }) => {
-    return(
-        <RegisterContext>
-            <UserHabits>
-                { children }
-            </UserHabits>
-        </RegisterContext>
-    )
-}
+  return (
+    <RegisterContext>
+      <LoginContext>
+        <UserHabits>
+          <ActivitiesContext>{children}</ActivitiesContext>
+        </UserHabits>
+      </LoginContext>
+    </RegisterContext>
+  );
+};
 
 export default ProvidersGathered;
