ba01d4a5fd0f9bb1676581f3470e75a5abd5bb74
Raissa Toledo <raissalst@gmail.com>
2021-10-18 16:44:43 -0300

    feat: user provider that provides userId and function that receives raw token to obtain userId

---
+++ src/Providers/user/index.js
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+
+export const UserContext = createContext([]);
+
+export const UserProvider = ({ children }) => {
+  const [userId, setUserId] = useState("");
+
+  const getUserId = (token) => {
+    const tokenDecoded = jwt_decode(token);
+    setUserId(JSON.stringify(tokenDecoded.user_id));
+  };
+
+  useEffect(() => {
+    const obtainToken = JSON.parse(localStorage.getItem("token"));
+
+    obtainToken && getUserId(obtainToken);
+  }, []);
+
+  //   console.log("user id no provider user", userId);
+
+  return (
+    <UserContext.Provider
+      value={{
+        userId,
+        getUserId,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
