8678f0e5067f72e320970bc73916118000d1efe6
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-18 14:45:06 -0300

    feat: dynamically rendering the card

--- src/Components/CardDashboard/index.jsx
+++ src/Components/CardDashboard/index.jsx
@@ -1,7 +1,16 @@
-export const CardDashboard = ({listType}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-} 
+import { useEffect, useState } from "react";
+
+export const CardDashboard = ({ listType }) => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    listType === "habit" ? setContent("Habitos") : setContent("Grupos");
+  },[listType]);
+
+  return (
+    <div>
+      <h1>{content}</h1>
+      <p>Você possui 0 {content}</p>
+    </div>
+  );
+};
