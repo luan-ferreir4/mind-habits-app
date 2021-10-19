3c0ed600224ef9586a6e6cd00302f91ec6084f4b
Roberto Rocha <rsantos.rocha@gmail.com>
2021-10-18 21:13:27 -0300

    refactor: split userGroupsProvider in two: groupsCommunity and userGroups

---
+++ src/Providers/userGroups/index.js
@@ -0,0 +1,124 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export const UserGroupsContext = createContext([]);
+
+export const UserGroupsProvider = ({ children }) => {
+  const [userGroups, setUserGroups] = useState([]);
+
+  const token = localStorage.getItem("token") || "";
+
+  const getSubscriptions = () => {
+    axios
+      .get("https://kenzie-habits.herokuapp.com/groups/subscriptions/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setUserGroups(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error(error.response.data.message);
+        } else {
+          // Something happened in setting up the request and triggered an Error
+          toast.error("Error", error.message);
+        }
+      });
+  };
+
+  const unsubscribeFromAGroup = (groupId) => {
+    axios
+      .delete(
+        `https://kenzie-habits.herokuapp.com/groups/${groupId}/unsubscribe/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        toast.success("Exclusão do grupo realizada com sucesso");
+        getSubscriptions();
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error(error.response.data.message);
+        } else {
+          // Something happened in setting up the request and triggered an Error
+          toast.error("Error", error.message);
+        }
+      });
+  };
+
+  const subscribeToAGroup = (groupId) => {
+    axios
+      .post(
+        `https://kenzie-habits.herokuapp.com/groups/${groupId}/subscribe/`,
+        null,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        toast.success(response.data.message);
+        getSubscriptions();
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error(`Group ${groupId} ${error.response.data.detail}`);
+        } else {
+          // Something happened in setting up the request and triggered an Error
+          toast.error("Error", error.message);
+        }
+      });
+  };
+
+  const editGroup = (groupId, payload) => {
+    axios
+      .patch(
+        `https://kenzie-habits.herokuapp.com/groups/${groupId}/`,
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        toast.success("Alteração realizada com sucesso");
+        getSubscriptions();
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error(error.response.data);
+        } else {
+          // Something happened in setting up the request and triggered an Error
+          toast.error("Error", error.message);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getSubscriptions();
+  }, []);
+
+  return (
+    <UserGroupsContext.Provider
+      value={{
+        userGroups,
+        unsubscribeFromAGroup,
+        getSubscriptions,
+        subscribeToAGroup,
+        editGroup,
+      }}
+    >
+      {children}
+    </UserGroupsContext.Provider>
+  );
+};
