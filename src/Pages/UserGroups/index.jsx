6e0a704d86b88630ddd62be646b78329c721fb29
Roberto Rocha <rsantos.rocha@gmail.com>
2021-10-19 14:44:37 -0300

    feat: initial commit

--- src/Pages/UserGroups/index.jsx
+++ src/Pages/UserGroups/index.jsx
@@ -1,9 +1,55 @@
+import { useContext } from "react";
+import { UserGroupsContext } from "../../Providers/userGroups";
+import {
+  Box,
+  Button,
+  Grid,
+  Card,
+  Typography,
+  CardActions,
+  CardContent,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
 const UserGroups = () => {
-  return ( 
-    <>
-        User Groups
-    </>
-  )
+  const { userGroups } = useContext(UserGroupsContext);
+  const history = useHistory();
+
+  return (
+    <Box>
+      <Typography variant="h3" component="div">
+        Meus Grupos
+      </Typography>
+      <Grid container spacing={3}>
+        {userGroups.map((group) => (
+          <Grid item xs={8} key={group.id}>
+            <Box>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {group.name}
+                  </Typography>
+                  <Typography sx={{ fontSize: 17 }}>
+                    {`Descrição: ${group.description}`}
+                  </Typography>
+                  <Typography sx={{ fontSize: 17 }}>
+                    {`Categoria: ${group.category}`}
+                  </Typography>
+                  <Box>
+                    <CardActions>
+                      <Button size="medium" color="primary" onClick={() => history.push("/groupdetails")}>
+                        Ver mais
+                      </Button>
+                    </CardActions>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default UserGroups;
