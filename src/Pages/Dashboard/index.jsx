54c96b30d34b60d691731cb39bca6865b60a35ee
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-20 19:40:43 -0300

    refactor: page UI

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,13 +1,28 @@
-import { NavBar } from "../../Components/NavBar";
-import { SideDock } from "../../Components/SideDock";
-import { CardDashboard } from "../../Components/CardDashboard";
 import {
   DashboardContent,
   DashboardMain,
   DashboardPage,
 } from "../../Styles/PagesStyle/DashboardPage";
 
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { UserContext } from "../../Providers/user";
+
+import { NavBar } from "../../Components/NavBar";
+import { SideDock } from "../../Components/SideDock";
+import { CardDashboard } from "../../Components/CardDashboard";
+
 const Dashboard = () => {
+  const [ userName, setUserName ] = useState("");
+
+  const { userId } = useContext(UserContext);
+
+  console.log(userId)
+
+  useEffect(()=>{
+    axios.get(`https://kenzie-habits.herokuapp.com/users/${userId}/`).then(res=>setUserName(res.data.username)).catch(err=>console.log(err.message))
+  });
+
   return (
     <DashboardPage>
       <NavBar typeNav="logged" />
@@ -16,7 +31,7 @@ const Dashboard = () => {
         <SideDock />
 
         <DashboardContent>
-          <h1>Olá, <span>user</span></h1>
+          <h1>Olá, <span>{userName}</span></h1>
 
           <div className="Card1">
             <p className="cardDescpt card--left">Organize seus hábitos e crie metas...</p>
--- src/Styles/PagesStyle/DashboardPage/index.js
+++ src/Styles/PagesStyle/DashboardPage/index.js
@@ -5,12 +5,12 @@ export const DashboardPage = styled.body`
 `;
 
 export const DashboardMain = styled.main`
-  height: 92%;
+  height: 91.2%;
 
   @media (min-width: 1024px) {
     display: flex;
     align-items: center;
-    height: 91.5%;
+    height: 90%;
   }
 `;
 
@@ -21,38 +21,43 @@ export const DashboardContent = styled.section`
     display: block;
     flex-grow: 1;
     height: 100%;
-
+    max-width: 1100px;
     h1 {
-        position: absolute;
-        top: 0;
-        left: 50%;
-        transform: translateX(-50%);
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
       font-size: var(--font-size-large);
       font-family: var(--font-type-details);
       text-align: center;
       span {
+        color: var(--font-primary-light);
         font-weight: var(--font-w-thin);
-        color: #f5ee;
       }
     }
+
     .cardDescpt {
       max-width: 400px;
       margin: 10px 0 0;
       font-family: var(--font-type-formal);
     }
+
     .card--right {
       text-align: right;
     }
+
     .Card1 {
-        position: absolute;
-        left: 5%;
-        top: 0;
-        width: 368px;
+      position: absolute;
+      left: 5%;
+      top: 10%;
+      width: 368px;
     }
+
     .Card2 {
-        position: absolute;
-        right: 10%;
-        width: 368px;
+      position: absolute;
+      right: 6%;
+      bottom: 2%;
+      width: 368px;
     }
   }
 `;
