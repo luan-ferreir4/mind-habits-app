7a7e888e0e3c0650456476f8c8747eb8c7540d0c
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-20 18:00:15 -0300

    feat: imported components

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,9 +1,41 @@
+import { NavBar } from "../../Components/NavBar";
+import { SideDock } from "../../Components/SideDock";
+import { CardDashboard } from "../../Components/CardDashboard";
+import {
+  DashboardContent,
+  DashboardMain,
+  DashboardPage,
+  DashboardHeader,
+} from "../../Styles/PagesStyle/DashboardPage";
+
 const Dashboard = () => {
   return (
-    <>
-        Dashboard
-    </>
-  )
+    <DashboardPage>
+      <NavBar typeNav="logged" />
+
+      <DashboardMain>
+        <SideDock />
+
+        <DashboardContent>
+          <h1>Olá, <span>user</span></h1>
+
+          <div className="Card1">
+            <p className="cardDescpt card--left">Organize seus hábitos e crie metas...</p>
+            <CardDashboard listType="habit" />
+          </div>
+
+          <div  className="Card2">
+            <p className="cardDescpt card--right">
+              ...Compartilhe e contribua com novos hábitos e metas por meio dos
+              grupos
+            </p>
+            <CardDashboard listType="group" />
+          </div>
+
+        </DashboardContent>
+      </DashboardMain>
+    </DashboardPage>
+  );
 };
 
 export default Dashboard;
