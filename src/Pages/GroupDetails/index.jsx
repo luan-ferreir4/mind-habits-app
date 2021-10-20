ad331791a2aa62752fe6a065c21aedf76ec3ca7e
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2021-10-20 10:33:13 -0300

    feat: GroupDetails started

--- src/Pages/GroupDetails/index.jsx
+++ src/Pages/GroupDetails/index.jsx
@@ -1,9 +1,24 @@
+import { NavBar } from "../../Components/NavBar";
+import { useState, useContext, useEffect } from "react";
+import { GroupsCommunityContext } from "../../Providers/groupsCommunity";
+
 const GroupDetails = () => {
+  const [specificGroup, setSpecificGroup] = useState({});
+  const { getASpecificGroup } = useContext(GroupsCommunityContext);
+  const groupId = localStorage.getItem("groupToRender") || "";
+
+  useEffect(() => {
+    setSpecificGroup(getASpecificGroup(115));
+  }, []);
+
+  console.log(specificGroup);
+
   return (
     <>
-        Group Details
+      <NavBar typeNav={"unlogged"} />
+      <h2>Equipe MindHabits</h2>
     </>
-  )
+  );
 };
 
 export default GroupDetails;
