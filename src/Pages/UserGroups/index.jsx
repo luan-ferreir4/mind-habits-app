49ae45a83a4337c25da80fba5a50295f772dca72
Roberto Rocha <rsantos.rocha@gmail.com>
2021-10-19 22:39:24 -0300

    feat: implementation of the user group page

--- src/Pages/UserGroups/index.jsx
+++ src/Pages/UserGroups/index.jsx
@@ -1,55 +1,68 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserGroupsContext } from "../../Providers/userGroups";
+
 import {
-  Box,
   Button,
-  Grid,
   Card,
-  Typography,
   CardActions,
   CardContent,
+  Grid,
+  Typography,
 } from "@material-ui/core";
+
 import { useHistory } from "react-router-dom";
+import { NavBar } from "../../Components/NavBar";
+import {
+  UserGroupsContainer,
+  UserGroupMenu,
+  MainContent,
+  CardMyGroups,
+} from "../../Styles/UserGroupsPage";
 
 const UserGroups = () => {
   const { userGroups } = useContext(UserGroupsContext);
   const history = useHistory();
 
   return (
-    <Box>
-      <Typography variant="h3" component="div">
-        Meus Grupos
-      </Typography>
-      <Grid container spacing={3}>
-        {userGroups.map((group) => (
-          <Grid item xs={8} key={group.id}>
-            <Box>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    {group.name}
-                  </Typography>
-                  <Typography sx={{ fontSize: 17 }}>
-                    {`Descrição: ${group.description}`}
-                  </Typography>
-                  <Typography sx={{ fontSize: 17 }}>
-                    {`Categoria: ${group.category}`}
-                  </Typography>
-                  <Box>
-                    <CardActions>
-                      <Button size="medium" color="primary" onClick={() => history.push("/groupdetails")}>
-                        Ver mais
-                      </Button>
-                    </CardActions>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    <>
+      <NavBar typeNav={"unlogged"} />
+      <UserGroupsContainer className="Container">
+        <UserGroupMenu className="Menu">
+          <img src="images/avatar.png" alt="Avatar" />
+          <div>Username</div>
+          <button onClick={() => history.push("/")}>Dashboard</button>
+          <button onClick={() => history.push("/habits")}>Meus Hábitos</button>
+          <button onClick={() => history.push("/usergroups")}>
+            Meus Grupos
+          </button>
+          <button onClick={() => history.push("/groups")}>Grupos</button>
+        </UserGroupMenu>
+        <MainContent>
+        <h3>Meus Grupos</h3>
+          {userGroups.map((group) => (
+            <div className="CardMyGroup" >
+              <Typography variant="h5" component="div">
+                {group.name}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }}>
+                {`Descrição: ${group.description}`}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }}>
+                {`Categoria: ${group.category}`}
+              </Typography>
+
+              <Button
+                size="medium"
+                color="primary"
+                onClick={() => history.push("/groupdetails")}
+              >
+                Ver mais
+              </Button>
+            </div>
+          ))}
+        </MainContent>
+      </UserGroupsContainer>
+    </>
   );
 };
-
 export default UserGroups;
