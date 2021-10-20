8324f93add72ba76f3b89422c803fbc25a2d8083
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-20 00:12:51 -0300

    refactor: imported styled component

--- src/Components/inputSelector/index.jsx
+++ src/Components/inputSelector/index.jsx
@@ -1,9 +1,11 @@
+import { Select } from "../../Styles/ComponentsStyle/InputSelector"
+
 export const InputSelector = ({selectType}) => {
     return (
-        <select>
+        <Select>
             {selectType === "category" ? (
                 <>
-                <option value="alimentação">alimentação</option>
+                <option value="alimentação" selected>alimentação</option>
                 <option value="fitness">fitness</option>
                 <option value="sono">sono</option>
                 <option value="meditação">meditação</option>
@@ -13,7 +15,7 @@ export const InputSelector = ({selectType}) => {
                 </>
             ): selectType === "dificulty" ? ( 
                 <>
-                <option value="muito fácil">muito fácil</option>
+                <option value="muito fácil" selected>muito fácil</option>
                 <option value="facil">facil</option>
                 <option value="médio">médio</option>
                 <option value="difícil">difícil</option>
@@ -22,13 +24,13 @@ export const InputSelector = ({selectType}) => {
             ): selectType === "time" ? (
                 <>
                 <option value="hora">hora</option>
-                <option value="dia">dia</option>
+                <option value="dia" selected>dia</option>
                 <option value="semana">semana</option>
                 <option value="mês">mês</option>
                 <option value="ano">ano</option>
                 </>
             ):(<></>)
             }
-        </select>
+        </Select>
     )
 }
