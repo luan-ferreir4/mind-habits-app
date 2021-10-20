91aed824e603cfbe0a941777b87bc1a1aad24cb7
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-20 20:39:29 -0300

    fix: sideDock import

--- src/Pages/GroupsCommunity/index.jsx
+++ src/Pages/GroupsCommunity/index.jsx
@@ -1,6 +1,6 @@
 import { NavBar } from "../../Components/NavBar";
 import { ButtonCreate } from "../../Components/Button-Create";
-import SideDock from "../../Components/SideDock";
+import { SideDock } from "../../Components/SideDock";
 import CardRender from "../../Components/CardRender";
 import { useContext } from "react";
 import { GroupsCommunityContext } from "../../Providers/groupsCommunity";
