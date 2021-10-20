c8acfb61df1883f06533cb8052e90dd9c9a76d97
Hiran Oliveira <oliveirahiran1@gmail.com>
2021-10-20 17:43:20 -0300

    feat: GroupsCommunityPage structure done

---
+++ src/Styles/PagesStyle/GroupsComunnityPage/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+`;
+
+export const Header = styled.header`
+  width: 25%;
+`;
+
+export const GroupMain = styled.main`
+  width: 70%;
+`;
+
+export const GroupSection = styled.section`
+  width: 25%;
+`;
