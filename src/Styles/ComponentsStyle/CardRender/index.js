bc58cc6f3029a1891a4f8593c5f7c0247f6e2bdf
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2021-10-21 11:46:23 -0300

    refactor: buttons updated

--- src/Styles/ComponentsStyle/CardRender/index.js
+++ src/Styles/ComponentsStyle/CardRender/index.js
@@ -91,3 +91,16 @@ export const CardGroup = styled.div`
     font-size: var(--font-size-standart);
   }
 `;
+
+export const ButtonMoreInfo = styled.button`
+  width: 149px;
+  height: 55px;
+  margin: 30px auto 20px;
+  background: #60b948;
+  color: #f5f5f5;
+  border: none;
+  border-radius: 10px;
+  font-size: 18px;
+  font-family: "Saira", sans-serif;
+  cursor: pointer;
+`;
