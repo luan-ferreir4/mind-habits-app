beb466110afc376d3dcfec2f7e311d3bff6007fb
Hiran Oliveira <oliveirahiran1@gmail.com>
2021-10-21 12:00:32 -0300

    fix: GroupCommunityPage style final fix

--- src/Styles/PagesStyle/GroupsComunnityPage/index.js
+++ src/Styles/PagesStyle/GroupsComunnityPage/index.js
@@ -1,17 +1,45 @@
 import styled from "styled-components";
 
-export const Main = styled.div`
+export const GComPage = styled.div`
+  height: 90vh;
+`;
+
+export const GComMain = styled.div`
+  height: 91.2%;
+  @media (min-width: 1024px) {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+`;
+
+export const GComContent = styled.div`
   display: flex;
-`;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0;
+  padding: 20px;
 
-export const Header = styled.header`
-  width: 25%;
-`;
+  h1 {
+    font-size: var(--font-size-xlarge);
+    font-family: var(--font-type-details);
+    font-weight: var(--font-w-thin);
+    text-align: center;
+    margin: 25px 0;
+  }
 
-export const GroupMain = styled.main`
-  width: 70%;
-`;
+  .divCards {
+    margin: 25px 0;
+  }
 
-export const GroupSection = styled.section`
-  width: 25%;
+  @media (min-width: 1024px) {
+    position: relative;
+    display: block;
+    .divCards {
+      height: 60vh;
+    }
+    .btnCreate {
+      padding-left: 680px;
+    }
+  }
 `;
