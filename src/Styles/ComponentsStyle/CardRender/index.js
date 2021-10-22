49da01b875ff615eb69b4c81371cebf1fe36ce81
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2021-10-21 23:42:22 -0300

    style: finished styling

--- src/Styles/ComponentsStyle/CardRender/index.js
+++ src/Styles/ComponentsStyle/CardRender/index.js
@@ -92,6 +92,50 @@ export const CardGroup = styled.div`
   }
 `;
 
+export const CardGroupCommunity = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 300px;
+
+  padding: 30px;
+  margin: 5px;
+  border-radius: 10px;
+
+  background-color: var(--green-pallet-three);
+  font-family: var(--font-type-details);
+
+  box-shadow: 2px 2px 3px rgb(0, 0, 0, 0.4), -1px -1px 7px rgb(0, 0, 0, 0.3);
+
+  .left {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .right {
+    margin-top: 20px;
+    width: 150px;
+  }
+  label {
+    width: 100%;
+  }
+  button {
+    width: 100%;
+    height: 40px;
+    margin: 3px;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    width: 100%;
+    border-radius: 25px;
+    padding: 10px 30px;
+    margin: 5px;
+    justify-content: space-between;
+
+    font-size: var(--font-size-standart);
+  }
+`;
+
 export const ButtonMoreInfo = styled.button`
   width: 149px;
   height: 55px;
@@ -103,4 +147,230 @@ export const ButtonMoreInfo = styled.button`
   font-size: 18px;
   font-family: "Saira", sans-serif;
   cursor: pointer;
+  :hover {
+    background-color: var(--green-pallet-two);
+  }
+`;
+
+export const CardGoal = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  padding: 30px;
+  margin: 5px;
+  width: 300px;
+  border-radius: 10px;
+
+  background-color: var(--green-pallet-three);
+  font-family: var(--font-type-details);
+
+  box-shadow: 2px 2px 3px rgb(0, 0, 0, 0.4), -1px -1px 7px rgb(0, 0, 0, 0.3);
+
+  h2 {
+    width: 100%;
+    text-align: center;
+    margin: 0px 0px 10px;
+    font-size: var(--font-size-medium);
+    line-height: 30px;
+  }
+
+  label {
+    width: 100%;
+  }
+
+  button {
+    width: 200px;
+    height: 40px;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    width: 400px;
+    margin: 10px;
+
+    h2 {
+      padding: 0px 10px 30px;
+      font-size: var(--font-size-large);
+      line-height: 50px;
+    }
+
+    label {
+      font-size: var(--font-size-medium);
+      margin: 10px 0px;
+    }
+  }
+`;
+
+export const CardActivity = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  padding: 30px;
+  margin: 5px;
+  width: 300px;
+  border-radius: 10px;
+
+  background-color: var(--green-pallet-three);
+  font-family: var(--font-type-details);
+
+  box-shadow: 2px 2px 3px rgb(0, 0, 0, 0.4), -1px -1px 7px rgb(0, 0, 0, 0.3);
+
+  h2 {
+    width: 100%;
+    text-align: center;
+    margin: 0px 0px 10px;
+    font-size: var(--font-size-medium);
+    line-height: 30px;
+  }
+
+  label {
+    width: 100%;
+    text-align: center;
+  }
+
+  button {
+    width: 200px;
+    height: 40px;
+    margin: 5px;
+  }
+
+  .buttonContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 20px;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    width: 400px;
+    margin: 10px;
+
+    h2 {
+      font-size: var(--font-size-large);
+      line-height: 35px;
+    }
+
+    label {
+      font-size: var(--font-size-standart);
+    }
+
+    .buttonContainer {
+      display: flex;
+      justify-content: center;
+      margin-top: 10px;
+    }
+
+    button {
+      width: 200px;
+      height: 40px;
+      margin: 5px 10px;
+    }
+  }
+`;
+
+export const CardHabit = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  padding: 30px;
+  margin: 5px;
+  width: 300px;
+  border-radius: 10px;
+
+  background-color: var(--green-pallet-three);
+  font-family: var(--font-type-details);
+
+  box-shadow: 2px 2px 3px rgb(0, 0, 0, 0.4), -1px -1px 7px rgb(0, 0, 0, 0.3);
+
+  h2 {
+    width: 100%;
+    text-align: center;
+    margin: 0px 0px 10px;
+    font-size: var(--font-size-medium);
+    line-height: 30px;
+  }
+
+  label {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin: 3px;
+    align-items: center;
+  }
+
+  span {
+    border-radius: 5px;
+    background-color: white;
+    width: 140px;
+    padding: 0px 5px;
+  }
+
+  .buttonContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 20px;
+  }
+
+  .bar {
+    width: 40px;
+    height: 30px;
+  }
+
+  button {
+    margin-top: 10px;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    width: 400px;
+    margin: 10px;
+
+    h2 {
+      font-size: var(--font-size-large);
+      line-height: 35px;
+    }
+
+    label {
+      font-size: var(--font-size-standart);
+    }
+
+    span {
+      width: 220px;
+      padding: 0px;
+    }
+
+    .buttonContainer {
+      display: flex;
+      justify-content: center;
+      margin-top: 10px;
+    }
+
+    button {
+      margin-top: 30px;
+      margin-bottom: 0px;
+    }
+  }
+`;
+
+export const ProgressButton = styled.button`
+  margin: 10px;
+
+  font-size: 20px;
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+
+  border: none;
+  border: 2px solid white;
+  color: white;
+  background-color: var(--green-pallet-three);
+  cursor: pointer;
+
+  @media only screen and (min-width: 1024px) {
+    width: 50px;
+    height: 50px;
+    font-size: 30px;
+  }
 `;
