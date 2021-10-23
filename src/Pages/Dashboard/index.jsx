dc422a6e6d2baebb23b5a3e605ed2243207e0c25
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-23 01:47:46 -0300

    refactor: verifyng if userId exists

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -15,46 +15,44 @@ const Dashboard = () => {
   const [userName, setUserName] = useState("");
 
   const { userId } = useContext(UserContext);
-  const token = localStorage.getItem("token") || "";
   useEffect(() => {
-    axios
-      .get(`https://kenzie-habits.herokuapp.com/users/${userId}/`, {
-        headers: { Authorization: `Bearer ${JSON.parse(token)}` },
-      })
-      .then((res) => setUserName(res.data.username))
-      .catch((err) => console.log(err.message));
-  });
+    if (userId) {
+      axios
+        .get(`https://kenzie-habits.herokuapp.com/users/${userId}/`)
+        .then((res) => setUserName(res.data.username))
+        .catch((err) => console.log(err.message));
+    }
+  }, [userId]);
 
   return (
     <>
       <NavBar typeNav="logged" />
-        <DashboardPage>
-          <div  className="sideDock">
+      <DashboardPage>
+        <div className="sideDock">
+          <SideDock />
+        </div>
 
-          <SideDock/>
+        <DashboardContent>
+          <h1>
+            Olá, <span>{userName}</span>
+          </h1>
+
+          <div className="Card1">
+            <p className="cardDescpt card--left">
+              Organize seus hábitos e crie metas...
+            </p>
+            <CardDashboard listType="habit" />
           </div>
 
-          <DashboardContent>
-            <h1>
-              Olá, <span>{userName}</span>
-            </h1>
-
-            <div className="Card1">
-              <p className="cardDescpt card--left">
-                Organize seus hábitos e crie metas...
-              </p>
-              <CardDashboard listType="habit" />
-            </div>
-
-            <div className="Card2">
-              <p className="cardDescpt card--right">
-                ...Compartilhe e contribua com novos hábitos e metas por meio
-                dos grupos
-              </p>
-              <CardDashboard listType="group" />
-            </div>
-          </DashboardContent>
-        </DashboardPage>
+          <div className="Card2">
+            <p className="cardDescpt card--right">
+              ...Compartilhe e contribua com novos hábitos e metas por meio dos
+              grupos
+            </p>
+            <CardDashboard listType="group" />
+          </div>
+        </DashboardContent>
+      </DashboardPage>
     </>
   );
 };
