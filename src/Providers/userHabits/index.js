2c4458b7eb8f47c4d8aef4d6d6238a4a1554969e
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2021-10-23 16:22:52 -0300

    fix: functions working correctly

--- src/Providers/userHabits/index.js
+++ src/Providers/userHabits/index.js
@@ -8,7 +8,7 @@ export const UserHabitsContext = createContext();
 export const UserHabitsProvider = ({ children }) => {
   const [habitsList, setHabitsList] = useState([]);
   const [notRenderd, setNotRenderd] = useState(false);
- 
+
   const { userId } = useContext(UserContext);
 
   const [token] = useState(() => {
@@ -34,65 +34,72 @@ export const UserHabitsProvider = ({ children }) => {
       });
   }, [token, habitsList]);
 
-
   const createHabit = (newHabit) => {
     const { title, category, difficulty, frequencyPartOne, frequencyPartTwo } =
       newHabit;
 
-    axios.post(
-      "https://kenzie-habits.herokuapp.com/habits/",
-      {
-        title: title,
-        category: category,
-        difficulty: difficulty,
-        frequency: `${frequencyPartOne} por ${frequencyPartTwo}`,
-        achieved: false,
-        how_much_achieved: 0,
-        user: userId,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+    axios
+      .post(
+        "https://kenzie-habits.herokuapp.com/habits/",
+        {
+          title: title,
+          category: category,
+          difficulty: difficulty,
+          frequency: `${frequencyPartOne} por ${frequencyPartTwo}`,
+          achieved: false,
+          how_much_achieved: 0,
+          user: userId,
         },
-      }
-    ).then(res=>{
-      toast.success("Hábito criado")
-    }).catch(err=>{
-      if (err.response) {
-        toast.error(
-          `Goals: ${err.response.status} ${err.response.statusText}`
-        );
-      } else {
-        toast.error("Error", err.message);
-      }
-    });
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        toast.success("Hábito criado");
+      })
+      .catch((err) => {
+        if (err.response) {
+          toast.error(
+            `Goals: ${err.response.status} ${err.response.statusText}`
+          );
+        } else {
+          toast.error("Error", err.message);
+        }
+      });
   };
 
   const updateHabit = (habitId, progress, isAchieved) => {
-    axios.patch(
-      `https://kenzie-habits.herokuapp.com/habits/${habitId}/`,
-      { how_much_achieved: progress, achieved: isAchieved },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    ).then(toast.success("Hábito atualizado")).catch(err=>{
-      if (err.response) {
-        toast.error(
-          `Goals: ${err.response.status} ${err.response.statusText}`
-        );
-      } else {
-        toast.error("Error", err.message);
-      }});
+    axios
+      .patch(
+        `https://kenzie-habits.herokuapp.com/habits/${habitId}/`,
+        { how_much_achieved: progress, achieved: isAchieved },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .catch((err) => {
+        if (err.response) {
+          toast.error(
+            `Goals: ${err.response.status} ${err.response.statusText}`
+          );
+        } else {
+          toast.error("Error", err.message);
+        }
+      });
   };
 
   const deleteHabit = (habitId) => {
-    axios.delete(`https://kenzie-habits.herokuapp.com/habits/${habitId}/`, {
-      headers: { Authorization: `Bearer ${token}` },
-    }).then(toast.success("Hábito deletado"));
+    axios
+      .delete(`https://kenzie-habits.herokuapp.com/habits/${habitId}/`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(toast.success("Hábito deletado"));
   };
 
   return (
@@ -102,7 +109,7 @@ export const UserHabitsProvider = ({ children }) => {
         notRenderd,
         createHabit,
         updateHabit,
-        deleteHabit
+        deleteHabit,
       }}
     >
       {children}
