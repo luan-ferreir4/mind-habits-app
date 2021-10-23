f2a634ffd3d3c4e076c4c5fa74f7816b24715f9b
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-23 15:29:10 -0300

    refactor: user provider, verifing if token exists

--- src/Providers/user/index.js
+++ src/Providers/user/index.js
@@ -2,18 +2,20 @@ import jwt_decode from "jwt-decode";
 
 import { createContext, useState, useEffect } from "react";
 
-export const UserContext = createContext([]);
+export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   
   const [userId, setUserId] = useState("");
-
-  const token = localStorage.getItem("token") || "";
+  
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    const tokenDecoded = jwt_decode(token);
-    setUserId(tokenDecoded.user_id);
-  }, [token]);
+    if(token){
+      const tokenDecoded = jwt_decode(token);
+      setUserId(tokenDecoded.user_id)
+    }
+  },[token]);
 
   return (
     <UserContext.Provider
