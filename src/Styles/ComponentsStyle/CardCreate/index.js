2c729f3a8b63f8f292bf373295f68e0a057abff4
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-24 17:25:52 -0300

    style: new layout for component

--- src/Styles/ComponentsStyle/CardCreate/index.js
+++ src/Styles/ComponentsStyle/CardCreate/index.js
@@ -1,64 +1,61 @@
 import styled from "styled-components";
 
 export const CardCreateContainer = styled.div`
-  /* display: flex;
-justify-content: space-around;
-flex-wrap: wrap;
-flex-direction: column; */
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 
-  margin: 0 auto;
-
+  height: 435px;
   width: 300px;
+  padding-top: 10px;
 
   background-color: var(--green-pallet-three);
   color: #000;
+  border-radius: 10px;
 
-  border-radius: 30px;
-
-  font-size: 16px;
   font-family: var(--font-type-details);
+  font-size: 16px;
 
-  input {
-    width: 100%;
-  }
-
-  label {
-    width: 100%;
-  }
   form {
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex-direction: column;
-    flex-wrap: wrap;
-    text-align: left;
-    /* border: 1px solid blue; */
-    margin: 100px 10px 0px;
     position: relative;
-    padding-top: 10px;
-
-    width: 90%;
-    height: 90%;
-
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    
+    width: 98%;
+    height: 100%;
+    text-align: center;
+    margin: 0 auto;
+    line-height: 32px;
+    
     .containerTitleAndInput {
       width: 100%;
       display: flex;
-      flex-direction: row;
+      flex-direction: column;
       flex-wrap: wrap;
-      justify-content: flex-start;
-      /* border: 1px solid red; */
+      justify-content: center;
+      align-items: center;
     }
 
     .frequencia {
       display: flex;
-      flex-direction: row;
       flex-wrap: wrap;
-      justify-content: flex-start;
-      /* border: 1px solid red; */
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
     }
 
     button {
-      margin: 20px auto;
+     position: absolute;
+     left: 50%;
+     transform: translateX(-50%);
+     bottom: 2.5%;
     }
   }
 `;
