fc39882a77229e96dd1ce8c2178ec9796591bdbc
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-25 17:09:33 -0300

    refactor: pages UI

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -3,26 +3,17 @@ import {
   DashboardContent,
 } from "../../Styles/PagesStyle/DashboardPage";
 
-import { useContext, useEffect, useState } from "react";
-import axios from "axios";
-import { UserContext } from "../../Providers/user";
+import { useContext } from "react";
+
+import { LoginContext } from "../../Providers/login";
 
 import { NavBar } from "../../Components/NavBar";
 import { SideDock } from "../../Components/SideDock";
 import { CardDashboard } from "../../Components/CardDashboard";
 
 const Dashboard = () => {
-  const [userName, setUserName] = useState("");
 
-  const { userId } = useContext(UserContext);
-  useEffect(() => {
-    if (userId) {
-      axios
-        .get(`https://kenzie-habits.herokuapp.com/users/${userId}/`)
-        .then((res) => setUserName(res.data.username))
-        .catch((err) => console.log(err.message));
-    }
-  }, [userId]);
+  const { userName } = useContext(LoginContext);
 
   return (
     <>
--- src/Styles/PagesStyle/GlobalPageStyle/index.js
+++ src/Styles/PagesStyle/GlobalPageStyle/index.js
@@ -1,4 +1,3 @@
-import { Container } from "@material-ui/core";
 import styled from "styled-components";
 
 export const Page = styled.div`
