4ed8d0c690d9fe844aedf982ad348a7a86ef31bb
Roberto Rocha <rsantos.rocha@gmail.com>
2021-10-25 20:36:31 -0300

    feat: component Alerts using Material UI

---
+++ src/Components/Alerts/index.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import MuiAlert from "@material-ui/lab/Alert";
+import { Snackbar } from "@material-ui/core";
+
+export const Alerts = ({ type, message }) => {
+  const [open, setOpen] = React.useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  return (
+    <>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={type} sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+};
+
+
